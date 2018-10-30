a9947d1afec2be8b963729deb053637bf8ccdd76
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-10-30 19:05:06 +0100

    added styling to UILabel

--- MySingleViewPlayground.playground/Contents.swift
+++ MySingleViewPlayground.playground/Contents.swift
@@ -12,10 +12,21 @@ class MyViewController : UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
         label.backgroundColor = .green
-        label.text = "Welcome to iOS Study Jam!"
+        label.text = "Welcome to iOS Study Jam!\nBrought to you by WTM Berlin"
         label.textColor = .black
         
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        
+        label.layer.shadowColor = UIColor.green.cgColor
+        label.layer.shadowRadius = 20.0
+        label.layer.shadowOpacity = 1.0
+        
+        label.layer.borderColor = UIColor.white.cgColor
+        label.layer.borderWidth = 1
+        
         view.addSubview(label)
+        
         label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
         label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
