37c7543003270afe70c6a68bcba0194e36ff0d92
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-10-30 20:21:03 +0100

    created new SingleApp project

---
+++ QuoteBox/QuoteBox/QuoteBox.swift
@@ -0,0 +1,21 @@
+//
+//  QuoteBox.swift
+//  QuoteBox
+//
+//  Created by Rohan Taneja on 30.10.18.
+//  Copyright © 2018 Rohan Taneja. All rights reserved.
+//
+
+import UIKit
+
+class QuoteBox: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ QuoteBox/QuoteBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuoteBox
+//
+//  Created by Rohan Taneja on 30.10.18.
+//  Copyright © 2018 Rohan Taneja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
