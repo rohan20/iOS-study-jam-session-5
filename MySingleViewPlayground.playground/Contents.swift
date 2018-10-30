0648d99a40f4b96be4241fbe49d30fb946e959c1
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-10-30 18:52:17 +0100

    centered the UIlabel

--- MySingleViewPlayground.playground/Contents.swift
+++ MySingleViewPlayground.playground/Contents.swift
@@ -9,11 +9,16 @@ class MyViewController : UIViewController {
         view.backgroundColor = .white
 
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
+        label.backgroundColor = .green
+        label.text = "Welcome to iOS Study Jam!"
         label.textColor = .black
         
         view.addSubview(label)
+        label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
         self.view = view
     }
 }
